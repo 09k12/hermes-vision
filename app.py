d57bbe362896a6c1e47661509ed6bd86cbc0db27
09k12 <165292291+09k12@users.noreply.github.com>
2024-05-03 12:22:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-# Copyright (c) Alibaba Cloud.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+
 import os
 # os.system('pip install tiktoken')
 # os.system('pip install "modelscope" --upgrade -f https://pypi.org/project/modelscope/')
